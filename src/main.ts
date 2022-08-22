98c2c71b2fbaf9a2cac3e1570348152c75c1132a
feyvik <vivianemma97@gmail.com>
2022-08-22 17:02:17 +0100

    Add package

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
+/** @format */
+
 import { createApp } from "vue";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Dialog from "primevue/dialog";
+import Button from "primevue/button";
+
+import "primevue/resources/themes/tailwind-light/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(PrimeVue)
+  .component("Dialog", Dialog)
+  .component("Button", Button)
+  .mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Button label="Submit" icon="pi pi-check" iconPos="right"></Button>
+    <Dialog></Dialog>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Button from 'primevue/button';
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
+   Button
   },
 });
 </script>
